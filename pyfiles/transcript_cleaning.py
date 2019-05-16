ff1319dff16481ab639144c4952d093aa741750e
justinho8161 <gujh816@gmail.com>
2019-05-16 10:55:15 -0400

    final without writeup

---
+++ pyfiles/transcript_cleaning.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import pyfiles.transcription_job 
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+import string
+import nltk
+nltk.download('wordnet')
+
+
+class TranscriptCleaning:
+    def __init__(self, status):
+        self.status = status
+        self.run()
+        self.tfidf_per_word = None
+
+    def get_word_tcs(self):
+        real_transcript = self.status['results']['items']
+        tc_and_words = []
+        for i in real_transcript:
+            if 'start_time' in i.keys():
+                tc_and_words.append([i['start_time'],i['end_time'],i['alternatives'][0]['content']])
+        self.tc_words = tc_and_words
+        return tc_and_words
+
+    def get_transcript(self):
+        self.transcript = self.status['results'][1][0]['transcript']
+        return self.transcript
+
+    def get_sentences_with_tcs(self):
+        separated_sentences = self.transcript.split(" ")
+        sentences_period = self.transcript.split(".")[0:-1]
+        end_sentence = [i for i, word in enumerate(separated_sentences) if "." in word]
+
+        tcs = []
+        start = 0
+        for i,j in enumerate(end_sentence):
+            tcs.append([(self.tc_words[start][0],self.tc_words[j][-2]),sentences_period[i]])
+            start = j+1
+        self.sentence_tc = tcs
+        return self.sentence_tc
+
+    def sentence_cleaning(self):
+        sentences_period = self.transcript.split(".")[0:-1]
+        wordnet_lemmatizer = WordNetLemmatizer()
+
+        translator = str.maketrans('', '', string.punctuation)
+
+        summary = []
+        for sentences in sentences_period:
+            summary.append(" ".join(
+                                    [wordnet_lemmatizer.lemmatize(word.translate(translator))
+                                    for word in sentences.lower().split(" ")
+                                    if word.translate(translator) not in self.stop_words]))
+        return summary, [[self.sentence_tc[i][0],summary[i]] for i in range(len(summary))]
+
+    def stopWords(self):
+        return set(list(
+        ["i'm","doh","el","isthe","thiss","ive","was","uh","wass","else","eh","it","i'll","ill","andi","wa","like","like","um","dont","thing","thought","kind","something","things","doesnt","didnt","theyll","dh","wa","a","about","above","after","again","against","ain","all","am","an","and","any","are","aren","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can","couldn","couldn't","d","did","didn","didn't","do","does","doesn","doesn't","doing","don","don't","down","during","each","few","for","from","further","had","hadn","hadn't","has","hasn","hasn't","have","haven","haven't","having","he","her","here","hers","herself","him","himself","his","how","i","if","in","into","is","isn","isn't","it","it's","its","itself","just","ll","m","ma","me","mightn","mightn't","more","most","mustn","mustn't","my","myself","needn","needn't","no","nor","not","now","o","of","off","on","oh","once","only","or","other","our","ours","ourselves","out","over","own","re","s","same","shan","shan't","she","she's","should","should've","shouldn","shouldn't","so","some","such","t","than","that","that'll","the","their","theirs","them","themselves","then","there","these","they","this","those","through","to","too","under","until","up","ve","very","was","wasn","wasn't","we","were","weren","weren't","what","when","where","which","while","who","whom","why","will","with","won","won't","wouldn","wouldn't","y","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves","could","he'd","he'll","he's","here's","how's","i'd","i'll","i'm","i've","let's","ought","she'd","she'll","that's","there's","they'd","they'll","they're","they've","we'd","we'll","we're","we've","what's","when's","where's","who's","why's","would","able","abst","accordance","according","accordingly","across","act","actually","added","adj","affected","affecting","affects","afterwards","ah","almost","alone","along","already","also","although","always","among","amongst","announce","another","anybody","anyhow","anymore","anyone","anything","anyway","anyways","anywhere","apparently","approximately","arent","arise","around","aside","ask","asking","auth","available","away","awfully","b","back","became","become","becomes","becoming","beforehand","begin","beginning","beginnings","begins","behind","believe","beside","besides","beyond","biol","brief","briefly","c","ca","came","cannot","can't","cause","causes","certain","certainly","co","com","come","comes","contain","containing","contains","couldnt","date","different","done","downwards","due","e","ed","edu","effect","eg","eight","eighty","either","else","elsewhere","end","ending","enough","especially","et","etc","even","ever","every","everybody","everyone","everything","everywhere","ex","except","f","far","ff","fifth","first","five","fix","followed","following","follows","former","formerly","forth","found","four","furthermore","g","gave","get","gets","getting","give","given","gives","giving","go","goes","gone","got","gotten","h","happens","hardly","hed","hence","hereafter","hereby","herein","heres","hereupon","hes","hi","hid","hither","home","howbeit","however","hundred","id","ie","im","immediate","immediately","importance","important","inc","indeed","index","information","instead","invention","inward","itd","it'll","j","k","keep","keeps","kept","kg","km","know","known","knows","l","largely","last","lately","later","latter","latterly","least","less","lest","let","lets","like","liked","likely","line","little","'ll","look","looking","looks","ltd","made","mainly","make","makes","many","may","maybe","mean","means","meantime","meanwhile","merely","mg","might","million","miss","ml","moreover","mostly","mr","mrs","much","mug","must","n","na","name","namely","nay","nd","near","nearly","necessarily","necessary","need","needs","neither","never","nevertheless","new","next","nine","ninety","nobody","non","none","nonetheless","noone","normally","nos","noted","nothing","nowhere","obtain","obtained","obviously","often","oh","ok","okay","old","omitted","one","ones","onto","ord","others","otherwise","outside","overall","owing","p","page","pages","part","particular","particularly","past","per","perhaps","placed","please","plus","poorly","possible","possibly","potentially","pp","predominantly","present","previously","primarily","probably","promptly","proud","provides","put","q","que","quickly","quite","qv","r","ran","rather","rd","readily","really","recent","recently","ref","refs","regarding","regardless","regards","related","relatively","research","respectively","resulted","resulting","results","right","run","said","saw","say","saying","says","sec","section","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sent","seven","several","shall","shed","shes","show","showed","shown","showns","shows","significant","significantly","similar","similarly","since","six","slightly","somebody","somehow","someone","somethan","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specifically","specified","specify","specifying","still","stop","strongly","sub","substantially","successfully","sufficiently","suggest","sup","sure","take","taken","taking","tell","tends","th","thank","thanks","thanx","thats","that've","thence","thereafter","thereby","thered","therefore","therein","there'll","thereof","therere","theres","thereto","thereupon","there've","theyd","theyre","think","thou","though","thoughh","thousand","throug","throughout","thru","thus","til","tip","together","took","toward","towards","tried","tries","truly","try","trying","ts","twice","two","u","un","unfortunately","unless","unlike","unlikely","unto","upon","ups","us","use","used","useful","usefully","usefulness","uses","using","usually","v","value","various","'ve","via","viz","vol","vols","vs","w","want","wants","wasnt","way","wed","welcome","went","werent","whatever","what'll","whats","whence","whenever","whereafter","whereas","whereby","wherein","wheres","whereupon","wherever","whether","whim","whither","whod","whoever","whole","who'll","whomever","whos","whose","widely","willing","wish","within","without","wont","words","world","wouldnt","www","x","yes","yet","youd","youre","z","zero","a's","ain't","allow","allows","apart","appear","appreciate","appropriate","associated","best","better","c'mon","c's","cant","changes","clearly","concerning","consequently","consider","considering","corresponding","course","currently","definitely","described","despite","entirely","exactly","example","going","greetings","hello","help","hopefully","ignored","inasmuch","indicate","indicated","indicates","inner","insofar","it'd","keep","keeps","novel","presumably","reasonably","second","secondly","sensible","serious","seriously","sure","t's","third","thorough","thoroughly","three","well","wonder"]))
+
+    def run(self):
+        self.stop_words = self.stopWords()
+        self.tc_words = self.get_word_tcs()
+        self.transcript = self.get_transcript()
+        self.sentence_tc = self.get_sentences_with_tcs()
+        self.summary, self.tc_and_clean = self.sentence_cleaning()
